7a14a9465dd9ae51b614ea6dbfc09e2eeeedecc7
laurakelly1 <lkelly.laura@gmail.com>
2022-09-16 19:10:51 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PromoAreaComponent } from './promo-area/promo-area.component';
+import { CatagoriesComponent } from './catagories/catagories.component';
+import { ProductsComponent } from './products/products.component';
+import { MailingListComponent } from './mailing-list/mailing-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PromoAreaComponent,
+    CatagoriesComponent,
+    ProductsComponent,
+    MailingListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/promo-area/promo-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-area',
+  templateUrl: './promo-area.component.html',
+  styleUrls: ['./promo-area.component.scss']
+})
+export class PromoAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
